b2e09d9a24c7b3d0a190cdc691cbb054737a9216
aeagonex <aeagonex@gmail.com>
2018-05-08 00:56:25 -0400

    Main Window Works Now

--- python/MainWindow.py
+++ python/MainWindow.py
@@ -1,13 +1,22 @@
 from os import getcwd
 
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout
 from PyQt5.uic import loadUi
+from PyQt5 import QtGui
+
+WIDTH = 800
+HEIGHT = 600
 
 class cuneiform(QMainWindow):
   def __init__(self):
     super().__init__()
     self.setWindowTitle('Cuneiform')
     self.setStyleSheet('background-color:#343434')
-    self.toolbar = self.addToolBar('Save')
-    self.setGeometry(50, 50, 800, 600)
+
+    self.setCentralWidget(QWidget(self))
+    self.hbox = QHBoxLayout()
+    self.centralWidget().setLayout(self.hbox)
+
+    self.setFixedSize(WIDTH, HEIGHT)
+
